61f6079c6d4711a7e62fba1a417686942ff62925
Steven Zeck <8315038+stevenzeck@users.noreply.github.com>
2022-01-26 23:33:49 -0600

    Updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,27 +1,17 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
-buildscript {
-    val kotlinVersion by extra("1.6.10")
-    val roomVersion by extra("2.4.1")
-    val hiltVersion by extra("2.40.5")
+val kotlinVersion by extra("1.6.10")
+val roomVersion by extra("2.4.1")
+val hiltVersion by extra("2.40.5")
 
-    repositories {
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        classpath("com.android.tools.build:gradle:7.1.0")
-        classpath("com.google.dagger:hilt-android-gradle-plugin:2.39.1")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
-
-        // NOTE: Do not place your application dependencies here; they belong
-        // in the individual module build.gradle files
-    }
+plugins {
+    id("com.android.application") version "7.1.0" apply false
+    id("com.android.library") version "7.1.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.6.10" apply false
 }
 
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
+buildscript {
+    dependencies {
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.39.1")
     }
 }
 
