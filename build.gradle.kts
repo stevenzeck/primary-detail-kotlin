854d48d8943693e8bb8cfd2499b36a383602809b
Steven Zeck <8315038+stevenzeck@users.noreply.github.com>
2022-04-24 21:30:36 -0500

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
-val kotlinVersion by extra("1.6.20")
+val kotlinVersion by extra("1.6.21")
 val roomVersion by extra("2.4.2")
 val hiltVersion by extra("2.41")
 val composeVersion by extra("1.2.0-alpha08")
@@ -8,7 +8,7 @@ val accompanistVersion by extra("0.24.7-alpha")
 plugins {
     id("com.android.application") version "7.1.3" apply false
     id("com.android.library") version "7.1.3" apply false
-    id("org.jetbrains.kotlin.android") version "1.6.20" apply false
+    id("org.jetbrains.kotlin.android") version "1.6.21" apply false
 }
 
 buildscript {
