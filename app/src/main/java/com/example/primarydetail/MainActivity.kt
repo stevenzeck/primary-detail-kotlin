69a12c633512a8fcdbfaabdc78cde8e93861f6a5
Steven Zeck <8315038+stevenzeck@users.noreply.github.com>
2025-05-22 19:02:24 -0500

    Get navigation3 somewhat working

--- app/src/main/java/com/example/primarydetail/MainActivity.kt
+++ app/src/main/java/com/example/primarydetail/MainActivity.kt
@@ -1,14 +1,24 @@
 package com.example.primarydetail
 
 import android.os.Bundle
+import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.material3.ExperimentalMaterial3Api
-import dagger.hilt.android.AndroidEntryPoint
-import androidx.navigation3.runtime.NavKey
-import androidx.navigation3.runtime.rememberNavBackStack
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.adaptive.ExperimentalMaterial3AdaptiveApi
+import androidx.compose.material3.adaptive.navigation3.ListDetailSceneStrategy
 import androidx.compose.material3.adaptive.navigation3.rememberListDetailSceneStrategy
+import androidx.navigation3.runtime.NavKey
+import androidx.navigation3.runtime.entry
+import androidx.navigation3.runtime.entryProvider
+import androidx.navigation3.runtime.rememberNavBackStack
+import androidx.navigation3.ui.NavDisplay
+import com.example.primarydetail.ui.postdetail.PostDetailScreen
+import com.example.primarydetail.ui.postlist.PostListScreen
+import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.serialization.Serializable
 
 @Serializable
@@ -20,14 +30,51 @@ data class PostDetail(val postId: Long) : NavKey
 @AndroidEntryPoint
 @ExperimentalFoundationApi
 @ExperimentalMaterial3Api
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
 
+    @OptIn(ExperimentalMaterial3AdaptiveApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
-            val backStack = rememberNavBackStack(PostList)
-            val listDetailStrategy = rememberListDetailSceneStrategy<NavKey>()
+
+            Scaffold(
+                topBar = {
+                    TopAppBar(
+                        title = {
+                            Text(text = "App")
+                        },
+                    )
+                },
+            ) { padding ->
+                val temp = padding
+                val backStack = rememberNavBackStack(PostList)
+                val listDetailStrategy = rememberListDetailSceneStrategy<NavKey>()
+
+                NavDisplay(
+                    backStack = backStack,
+                    onBack = { keysToRemove -> repeat(keysToRemove) { backStack.removeLastOrNull() } },
+                    sceneStrategy = listDetailStrategy,
+                    entryProvider = entryProvider {
+                        entry<PostList>(
+                            metadata = ListDetailSceneStrategy.listPane(
+                                detailPlaceholder = {
+                                    Text("Select something")
+                                }
+                            )
+                        ) {
+                            PostListScreen(onPostSelected = { postId ->
+                                backStack.add(PostDetail(postId = postId))
+                            })
+                        }
+                        entry<PostDetail>(
+                            metadata = ListDetailSceneStrategy.detailPane()
+                        ) { post ->
+                            PostDetailScreen()
+                        }
+                    }
+                )
+            }
         }
     }
 }
