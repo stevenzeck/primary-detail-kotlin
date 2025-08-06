a8dcbe3c66ec79d6efbc8cd2be0fd6448e67d50b
Steven Zeck <8315038+stevenzeck@users.noreply.github.com>
2025-08-05 23:42:32 -0500

    Update gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,12 +10,12 @@ plugins {
 }
 
 android {
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.primarydetail"
         minSdk = 21
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
