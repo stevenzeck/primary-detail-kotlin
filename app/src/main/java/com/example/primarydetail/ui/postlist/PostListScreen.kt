8c0528aa754e0160fcd1f39e36efffecf6696b7c
Steven Zeck <8315038+stevenzeck@users.noreply.github.com>
2025-09-13 13:36:06 -0500

    Fix issues

--- app/src/main/java/com/example/primarydetail/ui/postlist/PostListScreen.kt
+++ app/src/main/java/com/example/primarydetail/ui/postlist/PostListScreen.kt
@@ -23,9 +23,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import com.example.primarydetail.R // Assuming your string resources are in R
+import com.example.primarydetail.R
 import com.example.primarydetail.model.Post
 
 @ExperimentalFoundationApi
